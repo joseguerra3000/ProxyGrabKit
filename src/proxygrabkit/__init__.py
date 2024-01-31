4416cc3bdf276199e6a2d2fd64b345ea8cfa927c
Jose Guerra Carmenate <joseguerracarmenate@gmail.com>
2024-01-31 17:38:47 +0100

    Added __all__ variable on __init__.py

--- src/proxygrabkit/__init__.py
+++ src/proxygrabkit/__init__.py
@@ -1,5 +1,9 @@
 from .proxyrotator import RotatingProxyClient
 from .gimmeproxy import GimmeProxyClient
 
-__version__ = '0.1.0'
+__all__ = [RotatingProxyClient, GimmeProxyClient]
+
+
+__version__ = "0.1.0"
+
 VERSION = __version__
