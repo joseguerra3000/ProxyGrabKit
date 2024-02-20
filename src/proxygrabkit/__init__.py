0f4e8adcf181ecae2f18c26b699b51ececc85612
Jose Guerra Carmenate <joseguerracarmenate@gmail.com>
2024-02-20 18:45:53 +0100

    Modified package docstring

--- src/proxygrabkit/__init__.py
+++ src/proxygrabkit/__init__.py
@@ -1,7 +1,17 @@
+"""
+This package provides functionality for obtaining proxies from different sources.
+
+Classes
+-------
+    - :class:`.proxyrotator.RotatingProxyClient`
+    - :class:`.gimmeproxy.GimmeProxyClient`
+
+"""
+
 from .proxyrotator import RotatingProxyClient
 from .gimmeproxy import GimmeProxyClient
 
-__all__ = [RotatingProxyClient, GimmeProxyClient]
+__all__ = ['RotatingProxyClient', 'GimmeProxyClient']
 
 
 __version__ = "0.1.5"
